93007810efb56a9e73376369104de121f746448b
Rami <103028944+Rami24t@users.noreply.github.com>
2022-07-11 13:07:08 +0200

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,10 +1,10 @@
-determiner = 50;
-if(determiner>0)
-x= 30;
+determiner = -50;
+if(determiner<0)
+x= "Less than 0";
 else
-x=-30;
+x="Greater or equal to 0"
 
-let updater = "";
+updater = "";
 if(determiner>=0)
 {
 updater= "Greater or equal to 0";
@@ -19,5 +19,5 @@ console.log(updater);
 
 // We get 'undefined'
 
-//3. We use ternary operators to set a value to a variable, or to reduce code if necessary.  Use if-else statements for everything else. or if you only need if without else.
+//3.    When do you use a ternary v.s. an if statement? Give an example.    Use ternary operators to set a value to a variable, or to reduce code if necessary.  Use if-else statements for everything else. or if you only need if without else.
 
